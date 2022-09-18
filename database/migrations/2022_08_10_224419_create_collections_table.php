dab5c2a7808f245909f8603259508df71ae03698
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2022-09-18 20:26:33 +0200

    adding contracts ids on migrations

--- database/migrations/2022_08_10_224419_create_collections_table.php
+++ database/migrations/2022_08_10_224419_create_collections_table.php
@@ -10,6 +10,7 @@ return new class extends Migration {
         Schema::create('collections', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedInteger('collection_token_id');
             $table->text('description');
             $table->foreignId('category_id')->constrained();
             $table->string('logo_image');
