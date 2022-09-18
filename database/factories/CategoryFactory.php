9171e15c8f4ace0143710048118f4bc2c211a68e
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2022-09-18 22:33:27 +0200

    update categories seeders

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -12,11 +12,21 @@ class CategoryFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
     public function definition(): array
     {
+        $names = ["Arts", "Entertainment", "Music", "News", "Science", "Sports", "Technology",];
+        $icons = [
+            "http://127.0.0.1:8000/images/nfts/cat1.png",
+            "http://127.0.0.1:8000/images/nfts/cat2.png",
+            "http://127.0.0.1:8000/images/nfts/cat3.png",
+            "http://127.0.0.1:8000/images/nfts/cat4.png",
+            "http://127.0.0.1:8000//images/nfts/cat5.png",
+            "http://127.0.0.1:8000/images/nfts/cat6.png"
+        ];
         return [
-            'name' => fake()->name(),
-            'icon' => fake()->url(),
+            'name' => $names[rand(0, count($names) - 1)],
+            'icon' => $icons[rand(0, count($icons) - 1)],
         ];
     }
 }
